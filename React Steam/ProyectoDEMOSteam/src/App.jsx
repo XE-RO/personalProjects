31f0f733de5c28b8cc4af0fda1082ca16e82673e
XE-RO <delgadox0@gmail.com>
2023-12-21 07:21:13 -0600

    steamDEMOClone

---
+++ React Steam/ProyectoDEMOSteam/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from './components/Menu';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+export default App;
